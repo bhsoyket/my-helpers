99b8734f7a9ed0a7686afe4cd9fce59bd09da7ed
BelayetH <belayet@evaly.com.bd>
2022-01-01 17:35:49 +0600

    jwt helpers test added

---
+++ test/jwt.test.js
@@ -0,0 +1,26 @@
+const jwt = require('../src/helpers/jwt');
+const chai = require('chai');
+const { assert } = require('chai');
+
+
+console.log('JWT helpers test start.....');
+
+describe('JWT helpers testing', () => {
+    let createToken = '';
+    it('should create a token', async () => {            
+        createToken = await jwt.encode('secret_key', {name: 'test admin'});
+
+        assert.exists(createToken, 'Token is exist');
+        assert.isString(createToken, 'Token is string');
+    });
+
+    it('should varify a token', async () => {            
+        const varifyToken = await jwt.decode('secret_key', createToken);
+
+        assert.exists(varifyToken, 'Token is exist');
+        assert.isObject(varifyToken, 'Token is valid');
+        assert.isString(varifyToken.name, 'Name is string');
+    });
+});
+
+console.log('JWT helpers test end.....');
